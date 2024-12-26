2ab1a038fd499c9d3d76ead76430e1a225975881
Ramesh <rameshkittur.kn@gmail.com>
2024-12-26 16:53:00 +0530

    project initialization

---
+++ src/main/java/com/rameshkittur/jpaTutorials/JPA/entities/Product.java
@@ -0,0 +1,42 @@
+package com.rameshkittur.jpaTutorials.JPA.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(
+        name = "product",
+        uniqueConstraints = {
+                @UniqueConstraint(name ="sku_unique",columnNames = {"sku"})
+        },
+        indexes = {
+                @Index(name = "sku_index",columnList = "sku")
+        }
+)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sku;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
